f0d61cdd81dbba7201265324e0a3859ff4fa71d7
Rokyuto <viktorvalerievasenov6@gmail.com>
2022-08-11 22:41:19 +0300

    Edit UI

--- YouTueDownloader/graphic.py
+++ YouTueDownloader/graphic.py
@@ -7,19 +7,19 @@ from downloadVideo import *
 root= Tk()
 label_path=""
 root.geometry('500x500')
-select_dir=Label(root,text='select dir',font=('italic 14'))
+select_dir=Label(root,text='Select Directory',font=('italic 15'))
 select_dir.pack(pady=50)
 
 def get_directoryData():
     filepath=filedialog.askdirectory(initialdir='C:\Downloads',title='DirectoryPath')
-    label_path=Label(root,text=filepath,font=("italic 13"))
+    label_path=Label(root,text=filepath,font=("italic 20"))
     label_path.pack(pady=50)
     f_chooseSavePath(filepath)
 
-button=Button(root,text='getDir',command=get_directoryData,width=10,height=4,bg='red',fg='white')
+button=Button(root,text='Choose Directory',command=get_directoryData,width=20,height=4,bg='gray',fg='white')
 button.pack()
 
-videoLinkPath=Text(root,width=20 ,height=5,bg='red',fg="white")
+videoLinkPath=Text(root,width=40 ,height=5,bg='white',fg="black")
 videoLinkPath.pack(pady=2)
 content_type = "mp3"
 
@@ -35,6 +35,6 @@ def downloadVideo():
         print("Error")
         # Error Msg
 
-button=Button(root,text='getdata',command=downloadVideo,width=10,height=4,bg='red',fg='white')
+button=Button(root,text='Download',command=downloadVideo,width=20,height=4,bg='green',fg='white')
 button.pack()
 root.mainloop()
